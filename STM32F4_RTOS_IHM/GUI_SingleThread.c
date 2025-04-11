6864a6631c913f57cd9b832a3bdf84e78ec0dd00
N-rwal <104368706+N-rwal@users.noreply.github.com>
2025-04-11 09:27:39 +0200

    update CAN

---
+++ STM32F4_RTOS_IHM/GUI_SingleThread.c
@@ -0,0 +1,324 @@
+
+#ifdef _RTE_
+#include "RTE_Components.h"             // Component selection
+#endif
+
+#include "stm32f7xx_hal.h"
+#include "stm32746g_discovery_sdram.h"
+#include "RTE_Components.h"
+#include "cmsis_os.h"                   // ARM::CMSIS:RTOS:Keil RTX
+#include "Board_Touch.h"                // ::Board Support:Touchscreen
+#include "GUI.h"
+#include "stdio.h"
+#include "DIALOG.h"
+#include "Board_LED.h"                  // ::Board Support:LED
+#include "Board_Buttons.h"              // ::Board Support:Buttons
+#include "Driver_CAN.h"                 // ::CMSIS Driver:CAN
+
+
+#ifdef RTE_CMSIS_RTOS_RTX
+extern uint32_t os_time;
+
+uint32_t HAL_GetTick(void) { 
+  return os_time; 
+}
+#endif
+
+
+/*********************************************************************
+* *
+Externals
+* **********************************************************************
+*/
+WM_HWIN CreateWindow1(void);
+WM_HWIN CreateWindow2(void);
+WM_HWIN CreateWindow3(void);
+
+char windowInstance=0,sliderValue=0, GUI_proxyState = 0x00, GUI_light, GUI_moist;
+
+ARM_CAN_MSG_INFO   rx_msg_info;
+uint8_t CANdata_buf[8];
+/*----------------------------------------------------------------------------
+ *      GUIThread: GUI Thread for Single-Task Execution Model
+ *---------------------------------------------------------------------------*/
+
+extern ARM_DRIVER_CAN Driver_CAN1;//into the library !!
+void myCAN1_callback(uint32_t obj_idx, uint32_t event);
+void InitCan1 (void);
+
+void GUIThread (void const *argument);
+void myTime (void const *argument);
+void CANthreadR(void const *argument);
+void CANthreadT(void const *argument);
+
+osThreadId tid_GUIThread;
+osThreadId myTime_ID;
+osThreadId ID_CANthreadR;
+osThreadId ID_CANthreadT;
+
+osThreadDef(GUIThread, osPriorityIdle, 1, 2048);
+osThreadDef(myTime,osPriorityNormal, 1,0);
+osThreadDef(CANthreadR,osPriorityAboveNormal, 1,0);
+osThreadDef(CANthreadT,osPriorityNormal, 1,0);
+
+int Init_GUIThread (void) {
+
+  tid_GUIThread = osThreadCreate (osThread(GUIThread), NULL);
+  if (!tid_GUIThread) return(-1);
+  
+  return(0);
+}
+
+
+/**
+  * System Clock Configuration
+  *   System Clock source            = PLL (HSE)
+  *   SYSCLK(Hz)                     = 200000000
+  *   HCLK(Hz)                       = 200000000
+  *   AHB Prescaler                  = 1
+  *   APB1 Prescaler                 = 4
+  *   APB2 Prescaler                 = 2
+  *   HSE Frequency(Hz)              = 25000000
+  *   PLL_M                          = 25
+  *   PLL_N                          = 400
+  *   PLL_P                          = 2
+  *   PLL_Q                          = 8
+  *   VDD(V)                         = 3.3
+  *   Main regulator output voltage  = Scale1 mode
+  *   Flash Latency(WS)              = 6
+  */
+static void SystemClock_Config (void) {
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+
+  /* Enable HSE Oscillator and activate PLL with HSE as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 25;
+  RCC_OscInitStruct.PLL.PLLN = 400;  
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 8;
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+  /* Activate the OverDrive to reach the 200 MHz Frequency */
+  HAL_PWREx_EnableOverDrive();
+  
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;  
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;  
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_6);
+}
+
+
+
+/**
+  * Configure the MPU attributes
+  */
+static void MPU_Config (void) {
+  MPU_Region_InitTypeDef MPU_InitStruct;
+  
+  /* Disable the MPU */
+  HAL_MPU_Disable();
+
+  /* Configure the MPU attributes for SDRAM */
+  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+  MPU_InitStruct.BaseAddress = 0xC0200000;
+  MPU_InitStruct.Size = MPU_REGION_SIZE_2MB;
+  MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+  MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
+  MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
+  MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
+  MPU_InitStruct.Number = MPU_REGION_NUMBER0;
+  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL1;
+  MPU_InitStruct.SubRegionDisable = 0x00;
+  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
+
+  HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
+  /* Enable the MPU */
+  HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+}
+
+
+/**
+  * CPU L1-Cache enable
+  */
+static void CPU_CACHE_Enable (void) {
+
+  /* Enable I-Cache */
+  SCB_EnableICache();
+
+  /* Enable D-Cache */
+  SCB_EnableDCache();
+}
+
+/*********************************************************************
+*
+*       Main
+*/
+int main (void) {
+	
+	MPU_Config ();
+	CPU_CACHE_Enable();                       /* Enable the CPU Cache           */
+  
+  HAL_Init();                               /* Initialize the HAL Library     */
+	SystemClock_Config();                     /* Configure the System Clock     */
+	BSP_SDRAM_Init();                         /* Initialize BSP SDRAM           */
+	
+	osKernelInitialize ();                    // initialize CMSIS-RTOS
+	
+	
+	NVIC_SetPriority(CAN1_TX_IRQn,2);					//unknown
+  // initialize peripherals here
+	
+	LED_Initialize();
+	Buttons_Initialize();
+  // create 'thread' functions that start executing,
+	
+	Init_GUIThread();// verifier ordre de initialisation (horloge)
+	InitCan1();
+	//myTime_ID = osThreadCreate (osThread(myTime), NULL);
+	ID_CANthreadR = osThreadCreate (osThread(CANthreadR), NULL);
+	ID_CANthreadT = osThreadCreate (osThread(CANthreadT), NULL);
+  
+	
+  osKernelStart ();                         // start thread execution 
+	
+  osDelay(osWaitForever);
+}
+
+/*************************** End of file ****************************/
+//--------------------------------------------------------------------------------------------------CAN INIT
+void InitCan1 (void) {
+
+	Driver_CAN1.Initialize(NULL, myCAN1_callback);
+	Driver_CAN1.PowerControl(ARM_POWER_FULL);
+	
+	Driver_CAN1.SetMode(ARM_CAN_MODE_INITIALIZATION);//125000
+	Driver_CAN1.SetBitrate( ARM_CAN_BITRATE_NOMINAL,
+													125000,
+													ARM_CAN_BIT_PROP_SEG(5U)   |         // Set propagation segment to 5 time quanta
+                          ARM_CAN_BIT_PHASE_SEG1(1U) |         // Set phase segment 1 to 1 time quantum (sample point at 87.5% of bit time)
+                          ARM_CAN_BIT_PHASE_SEG2(1U) |         // Set phase segment 2 to 1 time quantum (total bit is 8 time quanta long)
+                          ARM_CAN_BIT_SJW(1U));                // Resynchronization jump width is same as phase segment 2
+	
+	Driver_CAN1.ObjectConfigure(0,ARM_CAN_OBJ_RX);				// Objet 0 du CAN1 pour r�ception, 2 pour la transmission
+	Driver_CAN1.ObjectConfigure(2,ARM_CAN_OBJ_TX);
+	Driver_CAN1.ObjectSetFilter( 0, ARM_CAN_FILTER_ID_EXACT_ADD , ARM_CAN_STANDARD_ID(0x0b6), 0) ;//KEIL LPC ID, for RX
+//	
+	Driver_CAN1.SetMode(ARM_CAN_MODE_NORMAL);					// fin init
+}
+//--------------------------------------------------------------------------------------------------CAN CALLBACK
+void myCAN1_callback(uint32_t obj_idx, uint32_t event)
+{
+	  switch (event)
+    {
+    case ARM_CAN_EVENT_RECEIVE:
+				Driver_CAN1.MessageRead(0, &rx_msg_info, CANdata_buf, 8);//8 byte max input
+        osSignalSet(ID_CANthreadR, (1<<10));
+    break;
+		case ARM_CAN_EVENT_SEND_COMPLETE:
+			osSignalSet(ID_CANthreadT, (1<<11));
+		break;
+    }
+}
+//--------------------------------------------------------------------------------------------------RANDOM
+void myTime(void const *argument)
+{
+	while(1)
+	{
+	}
+}
+//--------------------------------------------------------------------------------------------------CAN RECEIVE
+void CANthreadR(void const *argument)
+{
+  char i;
+  static char threshold = 20;
+	LED_Off (0);
+	while(1)
+	{
+		osSignalWait((1<<10),osWaitForever);
+    switch(rx_msg_info.id)
+    {
+      case 0x403:// uson sensors
+      for(i=0;i<5;i+++)
+      {
+        (CANdata_buf[i] > threshold) ? GUIproxyState | (1<<i) : GUIproxyState & (0<<i);
+      }
+      break;
+      case 0x402:// light + liquid ? battery
+      GUI_light = CANdata_buf[0];
+      GUI_moist = CANdata_buf[1];
+      break;
+      case 0x120://else
+
+      break;
+    }
+		}
+}
+//--------------------------------------------------------------------------------------------------CAN TRANSMIT
+void CANthreadT(void const *argument)
+{
+	ARM_CAN_MSG_INFO   tx_msg_info;
+	uint8_t data_buf[8];
+	tx_msg_info.rtr = 0; // 0 = data msg
+	
+	while(1)
+	{
+		osSignalWait((1<<12), osWaitForever);
+		tx_msg_info.id = ARM_CAN_STANDARD_ID (0x101);//High-priority periodic data
+		data_buf [0] = (char)(sliderValue * 255 / 100);//from the gui buttons
+		
+		Driver_CAN1.MessageSend(2, &tx_msg_info, data_buf, 1);
+		osSignalWait((1<<11), osWaitForever);
+		}
+}
+//--------------------------------------------------------------------------------------------------GUI THREAD
+void GUIThread (void const *argument) {
+	char user=1, userOld=0, state=1;
+	WM_HWIN hDlg;
+	
+
+  GUI_Init();
+	Touch_Initialize();
+
+	/* Add GUI setup code here */
+	hDlg = CreateWindow2();
+
+  while (1) {
+
+    /* All GUI related activities might only be called from here */
+		user = Buttons_GetState();
+		if((user && !userOld) || windowInstance)
+		{
+			windowInstance=0;
+			state++;
+			if(state==3)state=0;
+			
+			switch(state)
+			{
+				case 0:
+					hDlg = CreateWindow1();
+					break;
+				case 1:
+					hDlg = CreateWindow2();
+					break;
+				case 2:
+					hDlg = CreateWindow3();
+					break;
+			}
+		}
+		userOld=user;
+		
+		// mises � jour affichage
+		GUI_Exec();
+		GUI_Delay(10);
+		GUI_X_ExecIdle();             /* Nothing left to do for the moment ... Idle processing */
+  }
+}
