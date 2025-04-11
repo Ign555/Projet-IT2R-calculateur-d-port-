3896668bcdfafea9713275033c99b78497cd6931
ACay <adam.cayuela@etu-upsaclay.fr>
2025-04-11 02:42:18 +0200

    Add files via upload
    Add full CAN function

--- SMT RTOS IT2R/main.c
+++ SMT RTOS IT2R/main.c
@@ -408,38 +408,84 @@ void sensor_Task(void const*argument)
 	
 	
 
-void CAN_Task_Send(void const*argument)
-	{
-		char *ptr; 
-		ARM_CAN_MSG_INFO                tx_msg_info;
-		uint8_t tx_data_buf[8];
-		
+void CAN_Task_Send(void const* argument) {
+    ARM_CAN_MSG_INFO tx_msg_info;
+    uint8_t tx_data_buf[8];  // Buffer to hold the data to be sent
+    int *sensor_value;       // Pointer to the value received from sensor_Task
+    int *humidity_value;     // Pointer to the value received from Humidity_Task
+
+    tx_msg_info.id = ARM_CAN_STANDARD_ID(0x161);   /* ID for carburant & US */
+    tx_msg_info.rtr = 0;  // Not using RTR (Remote Transmission Request)
+
+    while (1) {
+        // Wait for the humidity value
+        osEvent humidity_event = osMailGet(ID_ADC_CAN, osWaitForever);
+        if (humidity_event.status == osEventMail) {
+            humidity_value = humidity_event.value.p;
+            // Copy humidity value into tx_data_buf (for example, you can use the first byte for humidity)
+            tx_data_buf[0] = *humidity_value;
+            osMailFree(ID_ADC_CAN, humidity_value);  // Free the memory for the humidity value
+
+            // Send humidity data
+            Driver_CAN2.MessageSend(2, &tx_msg_info, tx_data_buf, 1);  // Send 1 byte of humidity data
+            osSignalWait((1<<11), osWaitForever);  // Wait for send completion signal
+        }
+
+        // Wait for the sensor value
+        osEvent sensor_event = osMailGet(ID_sensor, osWaitForever);
+        if (sensor_event.status == osEventMail) {
+            sensor_value = sensor_event.value.p;
+            // Copy sensor value into tx_data_buf (for example, use first 4 bytes for sensor data)
+            for (int i = 0; i < 4; i++) {
+                tx_data_buf[i] = sensor_value[i];
+            }
+            osMailFree(ID_sensor, sensor_value);  // Free the memory for the sensor value
+
+            // Send sensor data
+            Driver_CAN2.MessageSend(2, &tx_msg_info, tx_data_buf, 4);  // Send 4 bytes of sensor data
+            osSignalWait((1<<11), osWaitForever);  // Wait for send completion signal
+        }
+    }
+}
 	
-		
-	tx_msg_info.id = ARM_CAN_STANDARD_ID(0x161);   /* pour carburant   */
-	tx_msg_info.rtr = 0;
-	tx_data_buf[0] = 0xAA;
-		
-		while(1)
-		{
-			osSignalWait((1<<12), osWaitForever);//on receive
-		
-		Driver_CAN2.MessageSend(2, &tx_msg_info, tx_data_buf, 1);
-		osSignalWait((1<<11), osWaitForever);
-			
-		}
-	}
-	
-	void CAN_Task_r (void const * argument){
-	
-	while (1)
-		{
-		
-		osSignalWait((1<<10), osWaitForever);
-		if(rx_msg_info.id & 0x101)osSignalSet(ID_CAN_Task_e, (1<<12));
-	}
-	
-	}
+	void CAN_Task_Receive(void const* argument) {
+    ARM_CAN_MSG_INFO rx_msg_info;
+    uint8_t rx_data_buf[8];  // Buffer to store the received data
+    int *led_data;           // Pointer to hold the received data for LED
+
+    rx_msg_info.id = ARM_CAN_STANDARD_ID(0x401);  // CAN ID 0x401 for the message filter
+    rx_msg_info.rtr = 0;  // Not using RTR (Remote Transmission Request)
+
+    // Enable CAN1 reception (assuming CAN1 is properly configured for receiving messages)
+    Driver_CAN1.MessageReceive(1, &rx_msg_info, rx_data_buf, 8);  // Receive up to 8 bytes of data from CAN1
+
+    while (1) {
+        // Wait for a CAN message on the CAN1 bus
+        osEvent can_event = osSignalWait((1 << 0), osWaitForever);  // Assume signal is set when message is received
+
+        if (can_event.status == osEventSignal) {
+            // Process received CAN message if ID matches 0x401
+            if (rx_msg_info.id == 0x401) {
+                // Copy the received data to led_data
+                led_data = osMailAlloc(LumDir, osWaitForever);  // Allocate memory for LED data from LumDir mailbox
+                if (led_data != NULL) {
+                    // Here, assume that the data corresponds to controlling the LED
+                    // Copy received data into the allocated mailbox object
+                    for (int i = 0; i < 8; i++) {
+                        led_data[i] = rx_data_buf[i];  // Copy the received message into the led_data
+                    }
+
+                    // Send the data to LED_Task through the LumDir mailbox
+                    osMailPut(LumDir, led_data);  // Send the LED data to the LED task's mailbox
+                }
+            }
+        }
+
+        // Wait again for the next message
+        osSignalWait((1 << 0), osWaitForever);
+    }
+}
+
 	
 	void Lum_Task ( void const * argument ){
 		
